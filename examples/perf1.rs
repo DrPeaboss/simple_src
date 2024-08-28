3c29b6d6a5f1540ea0407a2c5b3b9e551b15a80a
PeaZomboss <PeaZomboss@outlook.com>
2024-08-28 22:42:28 +0800

    Perf and flamegraph

---
+++ examples/perf1.rs
@@ -0,0 +1,17 @@
+use std::hint::black_box;
+
+use simple_src::{sinc, Convert};
+
+// cargo run --profile perf --example perf1
+// cargo flamegraph --profile perf --example perf1
+fn main() {
+    let now = std::time::Instant::now();
+    let manager = sinc::Manager::new(48000.0 / 44100.0, 150.0, 2048, 2050.0 / 22050.0);
+    println!("{:?}", now.elapsed());
+    let now = std::time::Instant::now();
+    let iter = (0..).map(|x| x as f64).into_iter();
+    for s in manager.converter().proc_iter(iter).take(48000) {
+        black_box(s);
+    }
+    println!("{:?}", now.elapsed());
+}

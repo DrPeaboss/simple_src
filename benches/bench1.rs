a373370cfa237adb83175b311dd6f8315d3d7f6c
PeaZomboss <PeaZomboss@outlook.com>
2024-10-15 14:54:14 +0800

    Change A=150 to A=144

--- benches/bench1.rs
+++ benches/bench1.rs
@@ -73,14 +73,14 @@ fn proc_a120_10ms(bencher: divan::Bencher, conv: &Conv) {
 }
 
 #[divan::bench(args=[Conv::C44k48k, Conv::C44k96k, Conv::C48k44k, Conv::C48k96k, Conv::C96k44k, Conv::C96k48k])]
-fn init_a150(conv: &Conv) -> Manager {
+fn init_a144(conv: &Conv) -> Manager {
     match conv {
-        Conv::C44k48k => Manager::new(R44K48K, 150.0, 2048, TRANS44K),
-        Conv::C44k96k => Manager::new(R44K96K, 150.0, 2048, TRANS44K),
-        Conv::C48k44k => Manager::new(R48K44K, 150.0, 2048, TRANS44K),
-        Conv::C48k96k => Manager::new(R48K96K, 150.0, 2048, TRANS48K),
-        Conv::C96k44k => Manager::new(R96K44K, 150.0, 2048, TRANS44K),
-        Conv::C96k48k => Manager::new(R96K48K, 150.0, 2048, TRANS48K),
+        Conv::C44k48k => Manager::new(R44K48K, 144.0, 2048, TRANS44K),
+        Conv::C44k96k => Manager::new(R44K96K, 144.0, 2048, TRANS44K),
+        Conv::C48k44k => Manager::new(R48K44K, 144.0, 2048, TRANS44K),
+        Conv::C48k96k => Manager::new(R48K96K, 144.0, 2048, TRANS48K),
+        Conv::C96k44k => Manager::new(R96K44K, 144.0, 2048, TRANS44K),
+        Conv::C96k48k => Manager::new(R96K48K, 144.0, 2048, TRANS48K),
     }
 }
 
@@ -88,8 +88,8 @@ fn init_a150(conv: &Conv) -> Manager {
     args=[Conv::C44k48k, Conv::C44k96k, Conv::C48k44k, Conv::C48k96k, Conv::C96k44k, Conv::C96k48k],
     sample_count=1000,
 )]
-fn proc_a150_10ms(bencher: divan::Bencher, conv: &Conv) {
-    let manager = init_a150(conv);
+fn proc_a144_10ms(bencher: divan::Bencher, conv: &Conv) {
+    let manager = init_a144(conv);
     let sample_num = conv.sample_num_10ms();
     bencher.bench_local(move || {
         let iter = (0..).map(|x| x as f64).into_iter();
